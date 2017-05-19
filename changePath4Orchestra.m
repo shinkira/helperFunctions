ad52fba6524791571ec845ee8e0b95b081e6bb56
shinkira <shinkira3@gmail.com>
2017-05-19 10:55:03 -0400

    createAcqBatch updated to change path to tif files locally by changePath4Orchestra

--- changePath4Orchestra.m
+++ changePath4Orchestra.m
@@ -1,9 +1,17 @@
-function OrchestraName = changePath4Orchestra(name)
+function OrchestraName = changePath4Orchestra(name,server_name)
     name = strrep(name,'\','/');
-    %%% save in the temporary folder (scratch2) ***
-    Imaging_ind = strfind(name,'Imaging');
-    OrchestraName = ['/n/scratch2/sk574/',name(Imaging_ind(1):end)];
-    %%% save in the permanent folder (data2) ***
-    % Shin_ind = strfind(name,'Shin');
-    % OrchestraName = ['/n/data2/hms/neurobio/harvey/',name(Shin_ind(1):end)];
+    switch server_name
+        case 'data2'
+            %%% save in the permanent folder (data2) ***
+            Shin_ind = strfind(name,'Shin');
+            OrchestraName = ['/n/data2/hms/neurobio/harvey/',name(Shin_ind(1):end)];
+        case 'scratch2'
+            %%% save in the temporary folder (scratch2) ***
+            Imaging_ind = strfind(name,'Imaging');
+            OrchestraName = ['/n/scratch2/sk574/',name(Imaging_ind(1):end)];
+        case 'no_backup'
+            %%% save in the temporary folder (no_backup) ***
+            Imaging_ind = strfind(name,'Imaging');
+            OrchestraName = ['/n/no_backup/harvey/Shin/',name(Imaging_ind(1):end)];
+    end
 end
--- createAcqBatch.m
+++ createAcqBatch.m
@@ -3,16 +3,24 @@ function createAcqBatch
 % mouse_set = [20,22,15];
 % date_set = {[161115],[161207],[161128,161222,161229]};
 
+% PPC
+mouse_set = [9,20,22,15];
+date_set = {[160516,160525,160608,160613],...
+            [161114,161117,161120,161130,161221,161229],...
+            [161129,161201],...
+            [161129,161205,161219,161221,161223,161227,170112,170116,170117]};
+        
+% V1 & RSP
 % mouse_set = [9,20,22,15];
-% date_set = {[160513,160517,160519,160520,160526],[161119,161206],[161205],[161125,161201,161216,161226]};
+% date_set = {[160513,160517,160519,160520,160526],...
+%             [161115,161119,161206],...
+%             [161205,161207],...
+%             [161125,161128,161201,161216,161222,161226,161229]};
 
-% mouse_set = [15];
-% date_set = {[161125,161201,161216,161226]};
-
-mouse_set = 35;
-date_set = {[170502,170503,170504,170505,...
-             170508,170509,170510,170511,170512,...
-             170515,170516,1705017]};
+% M1 & M2
+% date_set = {[170502,170503,170504,170505,...
+%              170508,170509,170510,170511,170512,...
+%              170515,170516,1705017]};
 
 save_dir = '\\research.files.med.harvard.edu\neurobio\HarveyLab\Shin\ShinDataAll\Imaging\BatchAcqObj';
 
@@ -49,16 +57,16 @@ for mi = 1:length(mouse_set)
             if ~exist('obj.motionCorrectionFunction','var')
                 % overwrite motion correction function
                 obj.motionCorrectionFunction = @lucasKanade_plus_nonrigid;
-                % switch mouse_num
-                %     case {1,3,16}
-                %         obj.motionCorrectionFunction = @withinFile_fullFrame_fft;
-                %     case {9,13,15,20,22,23,35}
-                %         obj.motionCorrectionFunction = @lucasKanade_affineReg;
-                % end
             else
                 obj.motionCorrectionFunction = motionCorrectionFunction;
             end
             fprintf('motionCorrectionFunction:\t%s\n',func2str(obj.motionCorrectionFunction));
+            % select the server_name: data2; scratch2; no_backup;
+            server_name = 'data2';
+            obj.defaultDir = changePath4Orchestra(obj.defaultDir,server_name);
+            for i = 1:length(obj.Movies)
+                obj.Movies{i} = changePath4Orchestra(obj.Movies{i},server_name);
+            end
             obj.save(save_dir)
         end
     end
