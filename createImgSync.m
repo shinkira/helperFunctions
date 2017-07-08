ba23c7adc8f13c5044099a652bdaa9d605758e61
shinkira <shinkira3@gmail.com>
2017-07-08 09:43:32 -0400

    createImgSync added to create a shell script to upload img data files to Orchestra

---
+++ createImgSync.m
@@ -0,0 +1,22 @@
+function createImgSync(mouse_num)
+    
+    initials = getInitials(mouse_num);
+    mouseID = sprintf('%s%03d',initials,mouse_num);
+    date_list = getDateListImg(mouse_num);
+        
+    save_dir = 'C:\Users\Shin\Documents\MATLAB\ShinCode\Imaging';
+    file_name = sprintf('imgSync2NoBackup_%s_test.sh',mouseID);
+    
+    fid = fopen(fullfile(save_dir,file_name),'w');
+    
+    for di = 1:size(date_list,1)
+        date_num = date_list{di,1};
+        msg = sprintf(['rsync -av /files/Neurobio/HarveyLab/Shin/ShinDataAll/Imaging/%s/%d/*.tif ',...
+            '/n/no_backup/neurobio/harvey/Shin/Imaging/%s/%d/'],mouseID,date_num,mouseID,date_num);
+    
+        fprintf(fid,[msg,'\n\n']);
+    end
+    
+    fclose(fid);
+
+end
