8f6aeb627bd87ff7d616b59e85b60e0aaacf5999
shinkira <shinkira3@gmail.com>
2017-07-28 11:30:32 -0400

    figPref added

---
+++ Shin/figPref.m
@@ -0,0 +1,13 @@
+function figPref
+    set(0,'defaultaxesfontsize',18);
+    set(0,'defaulttextfontsize',18);
+    set(0,'defaultaxesfontweight','bold');
+    set(0,'defaulttextfontweight','bold');
+    if 1
+        set(0,'defaultaxesfontsize',12);
+        set(0,'defaulttextfontsize',12);
+    end
+    set(0,'defaultaxestickdir','out');
+    set(0,'defaultaxesbox','off');
+    set(0,'defaultFigureColor','w');
+end
