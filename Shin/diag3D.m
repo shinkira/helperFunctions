12f34c01f57606fd0ae5f725595a667bd253e643
shinkira <shinkira3@gmail.com>
2018-02-27 17:56:07 -0500

    diag3D updated

--- Shin/diag3D.m
+++ Shin/diag3D.m
@@ -1,4 +1,7 @@
 function D = diag3D(W)
+    if isempty(W)
+        D = double.empty(0,size(W,1));
+    end
     [~,~,z_size] = size(W);
     for i = 1:z_size
         D(i,:) = diag(squeeze(W(:,:,i)));
