15791f02734ede1fbf7085b6b09538e9c0212983
shinkira <shinkira3@gmail.com>
2018-04-25 23:16:21 -0400

    createAcqBatch updated to use info_set_good_okay.mat

--- createAcqBatch.m
+++ createAcqBatch.m
@@ -5,56 +5,16 @@ if nargin<1
     % error('select the server_name: data2; scratch2; no_backup');
 end
 
-% ALL
-% mouse_set = [9,20,22,15,35];
-% date_set = {[160516,160525,160608,160613,160513,160517,160519,160520,160526],...
-%             [161114,161117,161120,161221,161229,161115,161119,161206],...
-%             [161129,161201,161205,161207],...
-%             [161129,161205,161219,161221,161223,161227,170112,170116,170117,161125,161128,161201,161216,161222,161226,161229],...
-%             [170502,170504,170505,170508,170509,170510,170511,170512,170516,170515,170517,170518,170519]};
 
-% PPC
-% mouse_set = [9,20,22,15];
-% date_set = {[160516,160525,160608,160613],...
-%             [161114,161117,161120,161130,161221,161229],...
-%             [161129,161201],...
-%             [161129,161205,161219,161221,161223,161227,170112,170116,170117]};
-        
-% V1 & RSP
-% mouse_set = [9,20,22,15];
-% date_set = {[160513,160517,160519,160520,160526],...
-%             [161115,161119,161206],...
-%             [161205,161207],...
-%             [161125,161128,161201,161216,161222,161226,161229]};
 
-% % M1 & M2
-% 
-mouse_set = [31];
-date_set = {[170922]};
+github_dir = 'C:\Users\Shin\Documents\GitHub\DmtsShared\DMTS';
 
-% mouse_set = [31];
-% date_set = {[170811,170815,170816,170817,170818,...
-%              170821,170822,170823,170824,170825,...
-%              170828,170829,170830,170831,170901,...
-%              170904,170905]};
-         
-% mouse_set = [45];
-% date_set = {[170822,170823,170824,170825,...
-%              170828,170829,170830,170831]};
-
-         
-
-% mouse_set = [35,31];
-% date_set = {[170515,170516,170517,170518,170519,170522],...
-%             [170623,170628,170629,170702,170703,170706,170707]};
-        
-% date_set = {[170502,170503,170504,170505,170508,170509,170510,170511,170512,...
-%              170515,170516,170517,170518,170519,170522]};
-         
-% % M1 & M2
-% mouse_set = 35;
-% date_set = {[999999]};
-
+load(fullfile(github_dir,'info_set_good_okay.mat'));
+mouse_set = [9,15,20,31,35,45];
+for mi = 1:length(mouse_set)
+    pick = cell2mat(info_set(:,1))==mouse_set(mi);
+    date_set{mi} = cell2mat(info_set(pick,2));
+end
 
 if ispc
     save_dir = '\\research.files.med.harvard.edu\Neurobio\HarveyLab\Tier2\Shin\ShinDataAll\Imaging\BatchAcqObj';
@@ -65,9 +25,10 @@ end
 for mi = 1:length(mouse_set)
     for di = 1:length(date_set{mi})
         mouse_num = mouse_set(mi);
+        mouseID = getMouseID(mouse_set(mi));
         initials = getInitials(mouse_num);
         date_num = date_set{mi}(di);
-        
+                
         if ismember(mouse_num,[22,23,35,31,45])
             FOV_list = {'FOV1_00001'};
         else
@@ -76,15 +37,20 @@ for mi = 1:length(mouse_set)
 
         if exist('mouse_num','var') && exist('date_num','var')
             if ispc
-                defaultDir = sprintf('\\\\research.files.med.harvard.edu\\Neurobio\\HarveyLab\\Tier2\\Shin\\ShinDataAll\\Imaging\\%s%03d\\%06d\\',...
-                    initials,mouse_num,date_num);
-            else
-                defaultDir = sprintf('/Volumes/Neurobio/HarveyLab/Shin/ShinDataAll/Imaging/%s%03d/%06d/',...
-                    initials,mouse_num,date_num);
+                root_dir = '\\research.files.med.harvard.edu';
+            elseif ismac
+                root_dir = '/Volumes';
             end
+            defaultDir = fullfile(root_dir,'Neurobio','HarveyLab','Tier2','Shin','ShinDataAll','Imaging',mouseID,num2str(date_num));
         else
             error('Essential variables are missing!')
         end
+        
+        if exist(fullfile(defaultDir,'Corrected'),'dir')
+            fprintf('Skipping %s %d\n',mouseID,date_num)
+            continue
+        end
+        
 
         for fi = 1:length(FOV_list)
             if iscell(FOV_list)
