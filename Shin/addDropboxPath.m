7867822e5088c1c499665da77ca619c41181fbb8
Shin Kira <shinkira3@gmail.com>
2018-06-05 10:12:47 -0400

    addDropboxPath updated for my new MacBook Pro

--- Shin/addDropboxPath.m
+++ Shin/addDropboxPath.m
@@ -5,7 +5,7 @@ function addDropboxPath
             dropboxPath = 'C:\Users\Shin\Dropbox (HMS)';
         case 'harveylabrig51'
             dropboxPath = 'C:\Users\Shin\Dropbox (HMS)';
-        case 'shinichiros-macbook-pro'
+        case 'shin-macbook-pro'
             dropboxPath = '/Users/shin/Dropbox (HMS)';
     end
     addpath(genpath(dropboxPath),'-END');
