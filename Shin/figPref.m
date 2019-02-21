23ddcc0c9990a0ee97cabcad392f586589abb6ca
shinkira <shinkira3@gmail.com>
2019-02-21 10:37:42 -0500

    figPref updated

--- Shin/figPref.m
+++ Shin/figPref.m
@@ -16,5 +16,12 @@ function figPref(font_size)
     set(0,'defaultaxestickdirmode','manual');
     set(0,'defaultaxesbox','off');
     set(0,'defaultFigureColor','w');
-    set(0, 'DefaultFigurePosition', [-750   600   560   420])
+    screen_size = get(groot,'MonitorPositions');
+    if size(screen_size,1)==2
+        size_h = 560;
+        size_v = 420;
+        x_pos = screen_size(1,1) + screen_size(1,3)/2 - size_h - 10;
+        y_pos = screen_size(1,2) + screen_size(1,4)/2 - size_v/2;
+        set(0, 'DefaultFigurePosition', [x_pos y_pos size_h size_v]);
+    end
 end
