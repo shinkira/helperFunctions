327f9b946b8324fe7e6f81e18f43b14e7aa6c74b
shinkira <shinkira3@gmail.com>
2019-02-21 10:38:15 -0500

    barwitherr updated not to show caps

--- Shin/barwitherr.m
+++ Shin/barwitherr.m
@@ -147,7 +147,7 @@ else
        x =  handles.bar.XData + [handles.bar.XOffset];
    end
     
-    hErrorbar = errorbar(mean(x,1), values, lowerErrors, upperErrors, '.k');
+    hErrorbar = errorbar(mean(x,1), values, lowerErrors, upperErrors, '.k','CapSize',0);
     set(hErrorbar, 'marker', 'none')
 end
 
