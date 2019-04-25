4d35ae806409d69da82708b9a5051d8e4bb4a120
shinkira <shinkira3@gmail.com>
2019-04-25 10:39:45 -0400

    transportFig code updated

--- Shin/transportFigPaper.m
+++ Shin/transportFigPaper.m
@@ -1,4 +1,4 @@
-function transportFigBatch(varargin)
+function transportFigPaper(varargin)
 
     if length(varargin)<1
         format = 'pdf';
@@ -13,33 +13,31 @@ function transportFigBatch(varargin)
         
     % Copy all figures to the transfer directory in PNG format
     switch getComputerName
-        case 'shin-macbook-pro'
-            figpath = '/Users/shin/Dropbox (HMS)/tempFigs/';
-        case 'harveylabrig51'
-            figpath = 'C:\Users\Shin\Documents\MATLAB\ShinData\Transfer\';
         case 'shin-pc'
-            figpath = 'E:\Dropbox (HMS)\tempFigs\';
+            dropbox_dir= 'E:\Dropbox (HMS)';
+        case 'shin-macbook-pro'
+            dropbox_dir = '/Users/shin/Dropbox (HMS)';        
     end
+    figpath = fullfile(dropbox_dir,'DmtsShin','tempFigs');
         
     h = findobj('type','figure');
     n = length(h);
     fprintf('Saving %d figures in %s...\n',n,format);
+    
     for i = 1:n
         fig_name = [headder,'_',num2str(h(i).Number)];
         figure(h(i).Number)
         set(gcf,'color','w')
         switch format
-            case 'eps'
-                % opt.fontswap = 0;
-                % print2eps([figpath,fig_name,'.eps'],gcf,opt);
-                print([figpath,fig_name,'.eps'],'-depsc')
-            case 'png'
-                export_fig(gcf,[figpath,fig_name,'.png'],'-png','-nocrop');
-            case 'jpg'
-                export_fig(gcf,[figpath,fig_name,'.jpg'],'-jpg','-nocrop');
             case 'pdf'
-                print([figpath,fig_name,'.pdf'],'-dpdf') 
-                % print([figpath,fig_name,'.pdf'],'-dpdf','-bestfit') 
+                print(fullfile(figpath,[fig_name,'.pdf']),'-dpdf') 
+            case 'eps'
+                % print2eps([figpath,fig_name,'.eps'],gcf,opt);
+                print(fullfile(figpath,[fig_name,'.eps']),'-depsc')
+            case 'png'
+                export_fig(gcf,fullfile(figpath,[fig_name,'.png']),'-png','-nocrop');
+            case 'jpg'
+                export_fig(gcf,fullfile(figpath,[fig_name,'.jpg']),'-jpg','-nocrop');
         end
     end
 end
