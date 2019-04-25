371a6dfa36215f532a66915250fec2e172bcfb44
shinkira <shinkira3@gmail.com>
2019-04-25 10:40:25 -0400

    fitPref updated to change the default axis color to Black

--- Shin/figPref.m
+++ Shin/figPref.m
@@ -26,4 +26,17 @@ function figPref(font_size)
         y_pos = screen_size(1,2) + screen_size(1,4)/2 - size_v/2;
         set(0, 'DefaultFigurePosition', [x_pos y_pos size_h size_v]);
     end
+    
+    % use get(groot, 'factory') to see all the default settings
+    default_color = [0 0 0];
+    set(0,'defaultAxesGridColor',default_color);
+    set(0,'defaultAxesXColor',default_color);
+    set(0,'defaultAxesYColor',default_color);
+    set(0,'defaultAxesZColor',default_color);
+    set(0,'defaultGeoaxesGridColor',default_color);
+    set(0,'defaultHistogram2EdgeColor',default_color);
+    set(0,'defaultPolaraxesGridColor',default_color);
+    set(0,'defaultPolaraxesRColor',default_color);
+    set(0,'defaultPolaraxesThetaColor',default_color);
+    
 end
